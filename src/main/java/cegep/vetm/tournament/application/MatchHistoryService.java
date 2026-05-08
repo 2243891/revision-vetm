6276a05bf875bdd4c7c551e6c9c73afe0e5a1a72
VincentVilain <vincent.vilain.1@ulaval.ca>
2026-05-07 23:50:24 -0400

    more

--- src/main/java/cegep/vetm/tournament/application/MatchHistoryService.java
+++ src/main/java/cegep/vetm/tournament/application/MatchHistoryService.java
@@ -8,4 +8,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MatchHistoryService {
+
+    public List<Match> findCompletedMatchesFor(Tournament tournament, Player player) {
+        List<Match> matches = new ArrayList<>();
+        for (Match match : tournament.getMatches()) {
+            if (match.hasResult()
+                    && (match.getPlayerOne().equals(player) || match.getPlayerTwo().equals(player))) {
+                matches.add(match);
+            }
+        }
+        return matches;
+    }
 }
