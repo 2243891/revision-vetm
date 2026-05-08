48d8675e31baf3edcced69bc3d6b3362be192335
VincentVilain <vincent.vilain.1@ulaval.ca>
2026-05-07 23:50:10 -0400

    wip

---
+++ src/main/java/cegep/vetm/tournament/application/MatchHistoryService.java
@@ -0,0 +1,11 @@
+package cegep.vetm.tournament.application;
+
+import cegep.vetm.tournament.domain.player.Player;
+import cegep.vetm.tournament.domain.tournament.Match;
+import cegep.vetm.tournament.domain.tournament.Tournament;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MatchHistoryService {
+}
